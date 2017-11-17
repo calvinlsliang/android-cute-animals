17c3d05a519cf5ab8d36960558ac6ef184dfebef
Calvin Liang <calvin@nextdoor.com>
2017-11-16 16:27:08 -0800

    Initial commit

---
+++ app/src/main/java/com/calvinlsliang/cuteanimals/MainFeed.kt
@@ -0,0 +1,12 @@
+package com.calvinlsliang.cuteanimals
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainFeed : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_feed)
+    }
+}
